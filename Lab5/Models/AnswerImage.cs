400e53467324d1ab16dc936695cfb508e16f1135
unknown <kkan0615@gmail.com>
2022-07-12 22:59:38 -0400

    :tada: first commit

---
+++ Lab5/Models/AnswerImage.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public enum Question
+    {
+        Earth, Computer
+    }
+
+    public class AnswerImage
+    {
+        public int AnswerImageId { get; set; }
+        [Required]
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+        [Required]
+        [Url]
+        public string Url { get; set; }
+        [Display(Name = "Question")]
+        public Question Question { get; set; }
+
+    }
+}
---
+++ Lab5/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Lab5.Data;
+using Microsoft.EntityFrameworkCore;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Routing;
+
+namespace Lab5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /* Define services here */
+            var connection = Configuration.GetConnectionString("DefaultDBConnection");
+            services.AddDbContext<AnswerImageDataContext>(options => options.UseSqlServer(connection));
+
+            var blobConnection = Configuration.GetConnectionString("AzureBlobStorage");
+            services.AddSingleton(new BlobServiceClient(blobConnection));
+
+            services.AddRazorPages();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
